671c4427fa96f91f3e2851236cbd521ef4b2377a
Jonathan Adly <gadly0123@gmail.com>
2024-10-31 15:09:28 -0400

    tests

--- colivara_py/async_client.py
+++ colivara_py/async_client.py
@@ -4,4 +4,4 @@ from typing import Optional
 class AsyncColiVara:
     # not implemented yet
     def __init__(self, base_url: Optional[str] = None, api_key: Optional[str] = None):
-        raise NotImplementedError("AsyncColivara is not implemented yet.")
+        raise NotImplementedError("AsyncColiVara is not implemented yet.")
